ad4fba0282a08140d4910d5038994a0cc27cf406
Hellayne <helene.phouratsamay@gmail.com>
2017-01-25 11:40:49 +0100

    Update Code_arduino_commente.ino

--- Code_arduino_commente.ino
+++ Code_arduino_commente.ino
@@ -30,10 +30,11 @@ void setup() {
     pinMode(greenLEDSaturne, OUTPUT); // Setting green LED for Saturne (2)
     pinMode(redLEDSaturne, OUTPUT); // Setting red LED for Saturne (2)
     
-    // WARNING : for the LED HIGH = level 0, LOW = level 1 (negative logic)
+
     // Value initialisation
     digitalWrite(outVenus,LOW); // to Venus : Saturne is not leader
     digitalWrite(outSaturne,LOW); // to Saturne : Venus is not leader
+    // WARNING : for the LED HIGH = level 0, LOW = level 1 (negative logic)
     digitalWrite(greenLEDVenus, HIGH);
     digitalWrite(redLEDVenus, HIGH);
     digitalWrite(greenLEDSaturne, HIGH);
@@ -45,51 +46,57 @@ void setup() {
 void loop() {
     
  switch (idLeader) {   
-    case 0: // Aucun leader, "initialisation"
+    case 0: // No leader, "initialisation" and recovery
         delay(1);
-        if (digitalRead(inVenus)==HIGH) {
-            idLeader=1;
-            digitalWrite(outSaturne,HIGH);
-            digitalWrite(outVenus,LOW);
+        if (digitalRead(inVenus)==HIGH) { // Venus changed its mode to Leader
+            idLeader=1; // Leader = Venus
+            digitalWrite(outSaturne,HIGH); // sending a signal to Saturne saying the leader is alive
+            digitalWrite(outVenus,LOW); // reseting the signal to Venus (unused when leader)
             digitalWrite(greenLEDVenus, LOW);
             digitalWrite(redLEDVenus, HIGH);
             
-        } else if (digitalRead(inSaturne)==HIGH) {
-            idLeader=2;
-            digitalWrite(outVenus,HIGH);
-            digitalWrite(outSaturne,LOW);
+        } else if (digitalRead(inSaturne)==HIGH) { // Saturne changed its mode to Leader
+            idLeader=2; // Leader = Saturne
+            digitalWrite(outVenus,HIGH); // sending a signal to Venus saying the leader is alive
+            digitalWrite(outSaturne,LOW); // reseting the signal to Saturne (unused when leader)
             digitalWrite(greenLEDSaturne, LOW);
             digitalWrite(redLEDSaturne, HIGH);
         }
     break; 
-    case 1:
+         
+    case 1: // Leader = Venus
+         // Waiting until the "I'm alive" signal, or the end of the counter
         while ((counter<D_COUNTER) && (digitalRead(inVenus)==LOW)) {
-            delay(1); // attente 1ms
+            delay(1); // delay 1ms
             counter=counter+1;
         }
-        if (counter==D_COUNTER) { // leader mort
+         
+        if (counter==D_COUNTER) { // leader dead (timeout, end of the counter)
             digitalWrite(greenLEDVenus, HIGH);
             digitalWrite(redLEDVenus, LOW);
             digitalWrite(outSaturne,LOW);
             digitalWrite(outVenus,HIGH);
             idLeader=0;
-        } else { // leader vivant
+        } else { // leader alive ("I'm alive" signal received)
             //digitalWrite(outSaturne,HIGH);
             digitalWrite(greenLEDVenus, LOW);
         } 
     break;
-    case 2:
+         
+    case 2: // Leader = Saturne
+         // Waiting until the "I'm alive" signal, or the end of the counter
         while ((counter<D_COUNTER) && (digitalRead(inSaturne)==LOW)) {
-            delay(1); // attente 1ms
+            delay(1); // delay 1ms
             counter=counter+1;
         }
-        if (counter==D_COUNTER) { // leader mort
+         
+        if (counter==D_COUNTER) { // leader dead (timeout, end of the counter)
             digitalWrite(greenLEDSaturne, HIGH);
             digitalWrite(redLEDSaturne, LOW);
             digitalWrite(outVenus,LOW);
             digitalWrite(outSaturne,HIGH);
             idLeader=0;
-        } else { // leader vivant
+        } else { // leader alive ("I'm alive" signal received)
             //digitalWrite(outVenus,HIGH);
             digitalWrite(greenLEDSaturne, LOW);
         }    
@@ -99,7 +106,7 @@ void loop() {
         break;
   }
     
-    counter=0;
+    counter=0; // counter reset
     
     
 }
